f70deaa720bdf43f44970729ff7ece5f97304482
George <georgediopan@gmail.com>
2025-06-10 16:31:44 +0300

    Initial add Search Bar

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,12 @@
 import MovieList from "@/components/MovieList/MovieList";
 import styles from "./page.module.css";
+import SearchBar from "@/components/SearchBar/SearchBar";
 
 export default function Home() {
   return (
     <div className={styles.page}>
       <h1>Movie Finder!</h1>
+      <SearchBar />
       <MovieList />
     </div>
   );
---
+++ src/components/SearchBar/SearchBar.js
@@ -0,0 +1,9 @@
+
+
+export default function SearchBar ()  {
+    return(
+        <div>
+            <h1>Hello</h1>
+        </div>
+    )
+}
